3e790464db4c60003c5bc053c42135748468e03b
Teffal <teffal@mail.ru>
2024-07-11 17:23:11 +0300

    fix bags and add method show_gistogram

--- python_advanced/data_loader.py
+++ python_advanced/data_loader.py
@@ -1,13 +1,9 @@
-import random
-import operator
-
-
 def generate_data(
         path_file: str, 
         number_start: int = 0, 
         number_end: int = 10, 
         count: int = 100, 
-        sign_operation: list[str] = ['+', '-', '*', '//']
+        sign_operation: list[str] = None
         ):
     """
     Create file 'path_file'.csv data set math example
@@ -17,22 +13,30 @@ def generate_data(
     param count: amount line in data set
     param sign_operation: list of basic math operation: +, -, *, /, **, %, //
     """
-    with open(path_file, 'w+', encoding='utf8') as file:
-        operators = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-            '/': operator.truediv,
-            '**': operator.pow,
-            '//': operator.floordiv,
-            '%': operator.mod
-        }
+
+    import random
+    import operator
+
+    sign_operation = sign_operation if sign_operation and type(sign_operation) == list else ['+', '-', '*', '/']
+
+    operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '**': operator.pow,
+        '//': operator.floordiv,
+        '%': operator.mod
+    }
+
+    with open(path_file, 'w', encoding='utf8') as file:
+        file.write(f'number_1,number_2,answer,operand\n')
         i = 0
         while i < count:
             number_1 = random.randint(number_start, number_end)
             number_2 = random.randint(number_start, number_end)
             operand = random.choice(sign_operation)
-            if operand not in '//%' and number_2 != 0:
+            if operand not in '//%' or number_2 != 0:
                 answer = operators[operand](number_1, number_2)
             else:
                 continue
@@ -41,5 +45,21 @@ def generate_data(
             i += 1
 
 
+def show_gistogram(path_file: str):
+    import pandas as pd
+    df = pd.read_csv(path_file)
+    # df['species'] = pd.Categorical.from_codes(y, target_names)
+
+    print(df.head())
+    # Визуализация при помощи matplotlib
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    # Построение парных графиков при помощи sns
+    sns.pairplot(df, hue='operand')
+    plt.show()
+
+
 if __name__ == '__main__':
-    generate_data('./python_advanced/examples.csv')
+    generate_data('examples.csv')
+    show_gistogramm('examples.csv')
